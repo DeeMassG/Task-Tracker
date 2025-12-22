d95a9012f4d8b61dac8637be721670f835470c5f
DeeMassG <DeeMassG@user.noreply.github.com>
2025-12-22 16:32:40 +0300

    обновляю конфиги для подключения к базе

--- app/config.py
+++ app/config.py
@@ -5,7 +5,4 @@ load_dotenv()
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    DB_SERVER = os.environ.get('DB_SERVER')
-    DB_USER = os.environ.get('DB_USER')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD')
-    DB_NAME = os.environ.get('DB_NAME')
+    DB_URL = os.environ.get('DB_URL')
